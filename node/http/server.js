9bd1039b791647f3918404f32afbe93810af65e7
paresh skill qode <skillqode.paresh@gmail.com>
2022-06-29 15:44:08 +0530

    290622

---
+++ node/http/server.js
@@ -0,0 +1,12 @@
+var http = require("http");
+console.log(http);
+var server = http.createServer((request, response) => {
+  response.setHeader("Content-type", "text/html");
+  // res.writeHead(200, { "Content-Type": "text/html" });
+  response.write("<h1> Hello </h1>");
+  console.log(" request rec.. ");
+  response.end();
+});
+server.listen(7878, () => {
+  console.log("Listning 7878");
+});
