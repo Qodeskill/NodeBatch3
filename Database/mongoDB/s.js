ebcf9a3a2ef483724529724ec3267d9a8b4f4269
paresh skill qode <skillqode.paresh@gmail.com>
2022-07-15 16:02:12 +0530

    commit 15/07/2022

--- Database/mongoDB/s.js
+++ Database/mongoDB/s.js
@@ -3,4 +3,4 @@
 // db.students.insertOne({ name: "Shiva", rollno: 103, marks: 100 });
 // db.students.insertOne({ name: "Pavan", rollno: 104, marks: 80 });
 // load("D:\student.js");
-db.students.find();
+// db.students.find();
